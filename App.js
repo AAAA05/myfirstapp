c84a1cbd9dad18507ec1ad100a2aed0d722ec587
unknown <264318241005-st@rmutsb.ac.tm>
2023-07-18 11:34:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View} from 'react-native'
+//import MyComponent from './my-component'
+import Variables from './variables'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App(){
+  /*return (
+      <MyComponent/>
+  );*/
+return(
+  <View style={{paddingTop:50, padding:20}}>
+      <Variables/>
+  </View>
+)
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
